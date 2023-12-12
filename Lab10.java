5882e12a044a84a36a15dfd4d18c15a51197934f
LargeBoiii <mikolaj.majka04@gmail.com>
2023-12-12 13:12:06 +0000

    Q2 redid month recognition into a switch statement

--- Lab10.java
+++ Lab10.java
@@ -55,9 +55,40 @@ public class Lab10 {
     public static void Q2() 
     {
         System.out.println("Q2: Enter the current day (1-31): ");
-        int num = Integer.parseInt(scan.nextLine());
+        int day = Integer.parseInt(scan.nextLine());
         System.out.println("Enter the current month: (1-12)");
-        int num2 = Integer.parseInt(scan.nextLine());
+        int month = Integer.parseInt(scan.nextLine());
+        String monthString = new String("");
+
+        switch(month)
+        {
+            case 1: monthString = "January";
+            break;
+            case 2: monthString = "February";
+            break;
+            case 3: monthString = "March";
+            break;
+            case 4: monthString = "April";
+            break;
+            case 5: monthString = "May";
+            break;
+            case 6: monthString = "June";
+            break;
+            case 7: monthString = "July";
+            break;
+            case 8: monthString = "August";
+            break;
+            case 9: monthString = "September";
+            break;
+            case 10: monthString = "October";
+            break;
+            case 11: monthString = "November";
+            break;
+            case 12: monthString = "December";
+            break;
+            default: System.out.println("Invalid month");
+        }
+
 
         
         
